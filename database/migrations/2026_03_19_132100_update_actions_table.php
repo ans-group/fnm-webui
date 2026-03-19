3008bfaeef3856ca99dd871abd6ca426477c9c25
Lee Spottiswood <me@lee.io>
2026-03-19 13:32:08 +0000

    remove deprecated fields for actions

---
+++ database/migrations/2026_03_19_132100_update_actions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('actions', function (Blueprint $table) {
+            $table->string('attack_detection_threshold')->nullable();
+            $table->string('attack_detection_threshold_direction')->nullable();
+            $table->dropColumn(['attack_type', 'attack_direction', 'attack_protocol', 'attack_initial_power', 'attack_peak_power']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('actions', function (Blueprint $table) {
+                $table->dropColumn(['attack_detection_threshold', 'attack_detection_threshold_direction']);
+                $table->string('attack_direction')->nullable();
+                $table->string('attack_type')->nullable();
+                $table->string('attack_protocol')->nullable();
+        });
+    }
+};
